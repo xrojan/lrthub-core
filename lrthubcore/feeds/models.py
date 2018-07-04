63d99ce8df618b5ed4dcd97fb4c3c5d22c062583
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-05 01:31:06 +0800

    Initial commit

--- lrthubcore/feeds/models.py
+++ lrthubcore/feeds/models.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Feed(models.Model):
+    type_id = models.IntegerField()
+    cover_image = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- lrthubcore/feeds/views.py
+++ lrthubcore/feeds/views.py
@@ -2,5 +2,34 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Feed
+
 
 # Create your views here.
+class FeedList(ListView):
+    model = Feed
+
+
+class FeedVied(DetailView):
+    model = Feed
+
+
+class FeedCreate(CreateView):
+    model = Feed
+    fields = ['type_id', 'cover_image', 'title', 'content', 'is_deleted']
+    success_url = reverse_lazy('feed_list')
+
+
+class FeedUpdate(UpdateView):
+    model = Feed
+    fields = ['type_id', 'cover_image', 'title', 'content', 'is_deleted']
+    success_url = reverse_lazy('feed_list')
+
+
+class FeedDelete(DeleteView):
+    model = Feed
+    success_url = reverse_lazy('feed_list')
