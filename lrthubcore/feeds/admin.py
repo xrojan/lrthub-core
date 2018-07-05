90a38550889b41c5171d3a8caf3635c4e9699abc
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-05 13:25:38 +0800

    Register models to admin

--- lrthubcore/feeds/admin.py
+++ lrthubcore/feeds/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Feed, FeedType
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(FeedType)
+admin.site.register(Feed)
