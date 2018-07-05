b644a0c00222acd96869d0dffebd5f87b381d1bb
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-06 00:33:02 +0800

    Add feed creation overrides

--- lrthubcore/feeds/api/views.py
+++ lrthubcore/feeds/api/views.py
@@ -6,9 +6,14 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 
-class FeedList(generics.ListAPIView):
+class FeedList(generics.ListCreateAPIView):
     queryset = Feed.objects.all()
     serializer_class = serializers.FeedSerializers
+
+    def create(self, request, *args, **kwargs):
+        super(FeedList, self).create(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK, "message": "Feed created successfully", "result": request.data}
+        return Response(response)
 
     def list(self, request, *args, **kwargs):
         serializer = serializers.FeedSerializers(self.get_queryset(), many=True)
