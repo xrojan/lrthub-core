04a506f702b9750431cbea74103bda0fcb132f76
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-06 17:39:11 +0800

    Add os root

--- lrthubcore/lrthubcore/settings.py
+++ lrthubcore/lrthubcore/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'lrthubcore.utils.custom_exception_handler',
