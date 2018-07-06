0968c4147d55d677ca44e68cf320564f7bbb752d
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-06 15:30:35 +0800

    Add custom response pagination

--- lrthubcore/feeds/api/views.py
+++ lrthubcore/feeds/api/views.py
@@ -2,6 +2,7 @@
 # @email code@jmdg.io
 from ..models import Feed, FeedType
 from . import serializers
+from . import pagination
 from rest_framework import generics, status
 from rest_framework.response import Response
 
@@ -9,16 +10,11 @@ from rest_framework.response import Response
 class FeedList(generics.ListCreateAPIView):
     queryset = Feed.objects.all()
     serializer_class = serializers.FeedSerializers
+    pagination_class = pagination.CustomPagination
 
     def create(self, request, *args, **kwargs):
         super(FeedList, self).create(request, args, kwargs)
         response = {"status_code": status.HTTP_200_OK, "message": "Successfully created", "result": request.data}
-        return Response(response)
-
-    def list(self, request, *args, **kwargs):
-        serializer = serializers.FeedSerializers(self.get_queryset(), many=True)
-        data = serializer.data
-        response = {"status_code": status.HTTP_200_OK, "message": "Success", "result": data}
         return Response(response)
 
 
--- lrthubcore/lrthubcore/settings.py
+++ lrthubcore/lrthubcore/settings.py
@@ -129,6 +129,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 JWT_AUTH = {
