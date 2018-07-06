51b32ec7265f0590fae740a783ae935619f8e414
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-06 17:07:41 +0800

    Update feed admin styles

--- lrthubcore/feeds/admin.py
+++ lrthubcore/feeds/admin.py
@@ -3,6 +3,22 @@ from __future__ import unicode_literals
 from .models import Feed, FeedType
 from django.contrib import admin
 
-# Register your models here.
-admin.site.register(FeedType)
-admin.site.register(Feed)
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    search_fields = ['type_id', 'title', 'content', ]
+    list_display = ('type_id', 'title', 'content')
+    list_filter = ('type_id', 'is_deleted',)
+
+
+@admin.register(FeedType)
+class FeedTypeAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    search_fields = ['name', ]
+    list_display = ('name', 'feed_count', 'created_on', 'updated_at')
+    list_filter = ('is_deleted',)
+
+    # noinspection PyMethodMayBeStatic
+    def feed_count(self, obj):
+        return obj.feed_set.count()
