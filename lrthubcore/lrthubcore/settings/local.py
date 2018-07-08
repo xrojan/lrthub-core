9ed7229ddf7f9a08238611679d6a8d5f70e5710d
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-08 17:20:46 +0800

    Update apps

--- lrthubcore/lrthubcore/settings/local.py
+++ lrthubcore/lrthubcore/settings/local.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_api_key',
     'company',
     'feeds',
