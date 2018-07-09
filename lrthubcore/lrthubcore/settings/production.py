83a1d57cab012e76c430ac20849296c765ecaea0
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-09 18:58:09 +0800

    Update production static files

--- lrthubcore/lrthubcore/settings/production.py
+++ lrthubcore/lrthubcore/settings/production.py
@@ -124,14 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
