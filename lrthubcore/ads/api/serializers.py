e0258c90032170c74df5e142bc045d0b31790c9f
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-14 14:40:03 +0800

    Update serializers

--- lrthubcore/ads/api/serializers.py
+++ lrthubcore/ads/api/serializers.py
@@ -8,5 +8,5 @@ from rest_framework import serializers
 class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertisement
-        fields = ('id', 'description', 'thumbnail', 'cover_image', 'video_url', 'view_hits', 'created_on', 'updated_at',
+        fields = ('id', 'description', 'interstitial_image', 'banner_ad_image', 'video_url', 'created_on', 'updated_at',
                   'is_deleted')
